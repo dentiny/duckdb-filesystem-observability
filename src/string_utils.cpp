a051125aec1e63e9ceb826322ba19e01e94a4dd7
dentiny <dentinyhao@gmail.com>
2025-09-17 05:37:24 +0000

    lint

--- src/string_utils.cpp
+++ src/string_utils.cpp
@@ -5,33 +5,33 @@
 namespace duckdb {
 
 namespace {
-const char* S3_PREFIX = "s3://";
-const char* GCS_PREFIX = "gs://";
+const char *S3_PREFIX = "s3://";
+const char *GCS_PREFIX = "gs://";
 
 // Precondition: [`filepath`] starts with [`s3://`].
-std::string GetS3Bucket(const std::string& filepath) {
-    std::string rest = filepath.substr(5);
-    auto pos = rest.find('/');
-    return (pos == std::string::npos) ? rest : rest.substr(0, pos);
+std::string GetS3Bucket(const std::string &filepath) {
+	std::string rest = filepath.substr(5);
+	auto pos = rest.find('/');
+	return (pos == std::string::npos) ? rest : rest.substr(0, pos);
 }
 
 // Precondition: [`filepath`] starts with [`gs://`].
-std::string GetGcsBucket(const std::string& filepath) {
-    std::string rest = filepath.substr(5);
-    auto pos = rest.find('/');
-    return (pos == std::string::npos) ? rest : rest.substr(0, pos);
+std::string GetGcsBucket(const std::string &filepath) {
+	std::string rest = filepath.substr(5);
+	auto pos = rest.find('/');
+	return (pos == std::string::npos) ? rest : rest.substr(0, pos);
 }
 
 } // namespace
 
-std::string GetObjectStorageBucket(const std::string& filepath) {
-    if (StringUtil::StartsWith(filepath, S3_PREFIX)) {
-        return GetS3Bucket(filepath);
-    }
-    if (StringUtil::StartsWith(filepath, GCS_PREFIX)) {
-        return GetGcsBucket(filepath);
-    }
-    return "";
+std::string GetObjectStorageBucket(const std::string &filepath) {
+	if (StringUtil::StartsWith(filepath, S3_PREFIX)) {
+		return GetS3Bucket(filepath);
+	}
+	if (StringUtil::StartsWith(filepath, GCS_PREFIX)) {
+		return GetGcsBucket(filepath);
+	}
+	return "";
 }
 
 } // namespace duckdb
--- unit/test_string_utils.cpp
+++ unit/test_string_utils.cpp
@@ -4,24 +4,24 @@
 #include "duckdb/common/string.hpp"
 #include "string_utils.hpp"
 
-using namespace duckdb;  // NOLINT
+using namespace duckdb; // NOLINT
 
 TEST_CASE("Get bucket name test", "[string utils test]") {
-    // Local filepath.
-    {
-        const string filepath = "/tmp/local/file";
-        REQUIRE(GetObjectStorageBucket(filepath) == "");
-    }
-    // S3 filepath.
-    {
-        const string filepath = "s3://bucket/directory/object";
-        REQUIRE(GetObjectStorageBucket(filepath) == "bucket");
-    }
-    // GCS filepath.
-    {
-        const string filepath = "gs://bucket/directory/object";
-        REQUIRE(GetObjectStorageBucket(filepath) == "bucket");
-    }
+	// Local filepath.
+	{
+		const string filepath = "/tmp/local/file";
+		REQUIRE(GetObjectStorageBucket(filepath) == "");
+	}
+	// S3 filepath.
+	{
+		const string filepath = "s3://bucket/directory/object";
+		REQUIRE(GetObjectStorageBucket(filepath) == "bucket");
+	}
+	// GCS filepath.
+	{
+		const string filepath = "gs://bucket/directory/object";
+		REQUIRE(GetObjectStorageBucket(filepath) == "bucket");
+	}
 }
 
 int main(int argc, char **argv) {
