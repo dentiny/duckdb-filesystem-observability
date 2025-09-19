85e2428d76e1f8711457649b15745b59313ba8cc
dentiny <dentinyhao@gmail.com>
2025-09-19 04:59:48 +0000

    lint

--- unit/test_quantile_estimator.cpp
+++ unit/test_quantile_estimator.cpp
@@ -11,47 +11,47 @@ using namespace duckdb; // NOLINT
 
 namespace {
 vector<int> GetRandomNumbers() {
-    constexpr size_t NUM_VALUE = 1000;
-    vector<int> v;
-    v.reserve(NUM_VALUE);
-    for (int i = 1; i <= NUM_VALUE; ++i) {
-        v.push_back(i);
-    }
-    std::random_device rd;
-    std::mt19937 g(rd());
-    std::shuffle(v.begin(), v.end(), g);
-    return v;
+	constexpr size_t NUM_VALUE = 1000;
+	vector<int> v;
+	v.reserve(NUM_VALUE);
+	for (int i = 1; i <= NUM_VALUE; ++i) {
+		v.push_back(i);
+	}
+	std::random_device rd;
+	std::mt19937 g(rd());
+	std::shuffle(v.begin(), v.end(), g);
+	return v;
 }
-}  // namespace
+} // namespace
 
 TEST_CASE("Quantile test", "[quantile test]") {
-    constexpr double MAX_TOLERABLE_DIFF = 10;
+	constexpr double MAX_TOLERABLE_DIFF = 10;
 
 	QuantileEstimator qe;
-    const auto values = GetRandomNumbers();
-    for (int cur_val : values) {
-        qe.Add(cur_val);
-    }
+	const auto values = GetRandomNumbers();
+	for (int cur_val : values) {
+		qe.Add(cur_val);
+	}
 
-    const double p50 = qe.p50();
-    REQUIRE(p50 >= 500 - MAX_TOLERABLE_DIFF);
-    REQUIRE(p50 <= 500 + MAX_TOLERABLE_DIFF);
+	const double p50 = qe.p50();
+	REQUIRE(p50 >= 500 - MAX_TOLERABLE_DIFF);
+	REQUIRE(p50 <= 500 + MAX_TOLERABLE_DIFF);
 
-    const double p75 = qe.p75();
-    REQUIRE(p75 >= 750 - MAX_TOLERABLE_DIFF);
-    REQUIRE(p75 <= 750 + MAX_TOLERABLE_DIFF);
+	const double p75 = qe.p75();
+	REQUIRE(p75 >= 750 - MAX_TOLERABLE_DIFF);
+	REQUIRE(p75 <= 750 + MAX_TOLERABLE_DIFF);
 
-    const double p90 = qe.p90();
-    REQUIRE(p90 >= 900 - MAX_TOLERABLE_DIFF);
-    REQUIRE(p90 <= 900 + MAX_TOLERABLE_DIFF);
+	const double p90 = qe.p90();
+	REQUIRE(p90 >= 900 - MAX_TOLERABLE_DIFF);
+	REQUIRE(p90 <= 900 + MAX_TOLERABLE_DIFF);
 
-    const double p95 = qe.p95();
-    REQUIRE(p95 >= 950 - MAX_TOLERABLE_DIFF);
-    REQUIRE(p95 <= 950 + MAX_TOLERABLE_DIFF);
+	const double p95 = qe.p95();
+	REQUIRE(p95 >= 950 - MAX_TOLERABLE_DIFF);
+	REQUIRE(p95 <= 950 + MAX_TOLERABLE_DIFF);
 
-    const double p99 = qe.p99();
-    REQUIRE(p99 >= 990 - MAX_TOLERABLE_DIFF);
-    REQUIRE(p99 <= 990 + MAX_TOLERABLE_DIFF);
+	const double p99 = qe.p99();
+	REQUIRE(p99 >= 990 - MAX_TOLERABLE_DIFF);
+	REQUIRE(p99 <= 990 + MAX_TOLERABLE_DIFF);
 }
 
 int main(int argc, char **argv) {
