d5214b95534f651860fab74dc03236d574017fd5
dentiny <dentinyhao@gmail.com>
2025-09-21 04:18:40 -0700

    Validate no repeated registration for one filesystem (#26)

--- src/filesystem_ref_registry.cpp
+++ src/filesystem_ref_registry.cpp
@@ -1,5 +1,6 @@
 #include "filesystem_ref_registry.hpp"
 
+#include "duckdb/common/exception.hpp"
 #include "observability_filesystem.hpp"
 
 namespace duckdb {
@@ -10,6 +11,20 @@ namespace duckdb {
 }
 
 void ObservabilityFsRefRegistry::Register(ObservabilityFileSystem *fs) {
+	auto* internal_filesystem = fs->GetInternalFileSystem();
+
+	// Validate internal filesystem is not another observability filesystem.
+	if (dynamic_cast<ObservabilityFileSystem*>(internal_filesystem) != nullptr) {
+		throw InvalidInputException("Cannot wrap an observabibility filesystem %s!", fs->GetName());
+	}
+ 
+	// Validate filesystem hasn't been registered before.
+	for (const auto& already_registered : observability_filesystems) {
+		if (already_registered->GetInternalFileSystem()->GetName() == internal_filesystem->GetName()) {
+			throw InvalidInputException("Cannot re-register the same internal filesystem %s!", internal_filesystem->GetName());
+		}
+	}
+
 	// TODO(hjiang): Add assertion that filesystem cannot be repeatedly registered.
 	observability_filesystems.emplace_back(fs);
 }
