46fa48a0909347c39c20dffb087915549d33c621
dentiny <dentinyhao@gmail.com>
2025-10-06 06:14:20 +0000

    fix logging

--- src/numeric_utils.cpp
+++ src/numeric_utils.cpp
@@ -11,7 +11,7 @@ constexpr double MAX_ERROR_RANGE = 0.01;
 
 bool IsDoubleEqual(double actual, double expected, bool print_if_unequal) {
 	const bool is_equal = std::fabs(actual - expected) <= MAX_ERROR_RANGE;
-	if (!is_equal) {
+	if (!is_equal && print_if_unequal) {
 		std::cerr << "Actual value = " << actual << ", expected value = " << expected << std::endl;
 	}
 	return is_equal;
