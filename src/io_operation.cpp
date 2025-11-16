abc4b35b0f452d3d1926abff44bb5a03794300a4
dentiny <dentinyhao@gmail.com>
2025-11-16 08:22:59 +0000

    no need io op cpp

--- src/io_operation.cpp
+++
@@ -1,7 +0,0 @@
-#include "include/io_operation.hpp"
-
-namespace duckdb {
-
-const std::array<const char *, kIoOperationCount> OPER_NAMES = {"open", "read", "list", "glob", "get_file_size"};
-
-} // namespace duckdb
