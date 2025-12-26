53db5425452fa9e0b8e3013c6ec795755efde813
dentiny <dentinyhao@gmail.com>
2025-12-25 19:34:16 -0800

    Fix compilation for incomplete query context (#69)

--- src/observability_filesystem.cpp
+++ src/observability_filesystem.cpp
@@ -1,6 +1,7 @@
 #include "observability_filesystem.hpp"
 
 #include "duckdb/common/string_util.hpp"
+#include "duckdb/main/client_context.hpp"
 #include "external_file_cache_stats_recorder.hpp"
 
 namespace duckdb {
